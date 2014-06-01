21df33e27e2097879bcac6cab955695d3fa46488
Edward Mead <emead006@gmail.com>
2014-06-01 01:21:41 +0000

    Added outline/method signatures to PWMClass.cpp

---
+++ PWMClass.cpp
@@ -0,0 +1,44 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include "PWMClass.h"
+
+using namespace std;
+
+PWMClass::PWMClass()
+{
+    this.pin = "P8_13"
+}
+
+PWMClass::PWMClass(string pin)
+{
+    this.pin = pin;
+}
+
+PWMClass::open_pwm()
+{
+    
+}
+
+PWMClass::close_pwm()
+{
+
+}
+
+PWMClass::set_duty(int duty)
+{
+
+}
+
+PWMClass::set_period(int duty)
+{
+
+}
+
+PWMClass::set_run(int duty)
+{
+
+}
+
+
