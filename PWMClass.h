26802c7cf4fac190b10adec8cb6a5634ff7e2b5e
Edward Mead <emead006@gmail.com>
2014-06-01 01:23:22 +0000

    Changed setup_pwm to open_pwm, added close_pwm

--- PWMClass.h
+++ PWMClass.h
@@ -9,7 +9,8 @@ class PWMClass {
         PWMClass(); //create a pwm class object with the default pin of P8_13
         PWMClass(string pin); //create a pwm class object with the specified pin
 
-        int setup_pwm(); //initialize pwm
+        int open_pwm(); //initialize/open pwm
+        int close_pwm(); //close pwm
         int set_duty(int duty); //set the PWM duty cycle
         int set_period(int period); //set the PWM period
         int set_run(bool runstate); //set whether the pwm is running (0/1)
